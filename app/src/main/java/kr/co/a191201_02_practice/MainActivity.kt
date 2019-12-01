42da91837718ac62eb3ac826454013c028cd8d49
CUdex9779 <cuyu9779@gmail.com>
2019-12-01 12:30:24 +0900

    로그인 화면 구성

--- app/src/main/java/kr/co/a191201_02_practice/MainActivity.kt
+++ app/src/main/java/kr/co/a191201_02_practice/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.a191201_02_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+        loginBtn.setOnClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = passEdt.text.toString()
+
+            if (inputId == "admin" && inputPw = "qw123"){
+                Toast.makeText(this , "관리자입니다." , Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this , "일반사용자입니다." , Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
 
